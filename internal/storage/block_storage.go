7957338684f842494ecb44911ecbc848bebfba9e
Patrick O'Grady <prohb125@gmail.com>
2020-05-13 08:12:14 -0700

    Don't hash keys in DB so prefix scan possible

--- internal/storage/block_storage.go
+++ internal/storage/block_storage.go
@@ -17,7 +17,6 @@ package storage
 import (
 	"bytes"
 	"context"
-	"crypto/sha256"
 	"encoding/gob"
 	"encoding/json"
 	"errors"
@@ -51,6 +50,9 @@ const (
 
 	// balanceNamespace is prepended to any stored balance.
 	balanceNamespace = "balance"
+
+	// blockNamespace is prepended to any stored block.
+	blockNamespace = "block"
 )
 
 var (
@@ -87,40 +89,27 @@ var (
   Key Construction
 */
 
-// hashBytes is used to construct a SHA1
-// hash to protect against arbitrarily
-// large key sizes.
-func hashBytes(data string) []byte {
-	h := sha256.New()
-	_, err := h.Write([]byte(data))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return h.Sum(nil)
-}
-
 func getHeadBlockKey() []byte {
-	return hashBytes(headBlockKey)
+	return []byte(headBlockKey)
 }
 
 func getBlockKey(blockIdentifier *types.BlockIdentifier) []byte {
-	return hashBytes(
-		fmt.Sprintf("%s:%d", blockIdentifier.Hash, blockIdentifier.Index),
+	return []byte(
+		fmt.Sprintf("%s/%s/%d", blockNamespace, blockIdentifier.Hash, blockIdentifier.Index),
 	)
 }
 
 func getHashKey(hash string, isBlock bool) []byte {
 	if isBlock {
-		return hashBytes(fmt.Sprintf("%s:%s", blockHashNamespace, hash))
+		return []byte(fmt.Sprintf("%s/%s", blockHashNamespace, hash))
 	}
 
-	return hashBytes(fmt.Sprintf("%s:%s", transactionHashNamespace, hash))
+	return []byte(fmt.Sprintf("%s/%s", transactionHashNamespace, hash))
 }
 
 // GetBalanceKey returns a deterministic hash of an types.Account + types.Currency.
 func GetBalanceKey(account *types.AccountIdentifier, currency *types.Currency) []byte {
-	return hashBytes(
+	return []byte(
 		fmt.Sprintf("%s/%s/%s", balanceNamespace, types.Hash(account), types.Hash(currency)),
 	)
 }
