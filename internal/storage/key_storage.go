7bcde76878f9ca8628042ed0cd25d7346fa9127c
Patrick O'Grady <prohb125@gmail.com>
2020-07-22 17:06:45 -0700

    Nits

--- internal/storage/key_storage.go
+++ internal/storage/key_storage.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Coinbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package storage
 
 import (
@@ -140,7 +154,10 @@ func (k *KeyStorage) GetAllAddresses(ctx context.Context) ([]string, error) {
 }
 
 // Sign attempts to sign a slice of *types.SigningPayload with the keys in KeyStorage.
-func (k *KeyStorage) Sign(ctx context.Context, payloads []*types.SigningPayload) ([]*types.Signature, error) {
+func (k *KeyStorage) Sign(
+	ctx context.Context,
+	payloads []*types.SigningPayload,
+) ([]*types.Signature, error) {
 	signatures := make([]*types.Signature, len(payloads))
 	for i, payload := range payloads {
 		keyPair, err := k.Get(ctx, payload.Address)
