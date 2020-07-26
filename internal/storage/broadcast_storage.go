5219a326cd7d36dad2a48ba5c65215ad69436ac7
Patrick O'Grady <prohb125@gmail.com>
2020-07-26 09:48:55 -0700

    Add more comments

--- internal/storage/broadcast_storage.go
+++ internal/storage/broadcast_storage.go
@@ -34,20 +34,40 @@ type BroadcastStorage struct {
 	staleDepth        int64
 }
 
+// BroadcastStorageHelper is used by BroadcastStorage to submit transactions
+// and find said transaction in blocks on-chain.
 type BroadcastStorageHelper interface {
-	CurrentBlockIdentifier(context.Context) (*types.BlockIdentifier, error)             // used to determine if should rebroadcast
-	FindTransaction(context.Context, *types.TransactionIdentifier) (int64, error)       // used to confirm
+	// CurrentBlockIdentifier is called before transaction broadcast and is used
+	// to determine if a transaction broadcast is stale.
+	CurrentBlockIdentifier(context.Context) (*types.BlockIdentifier, error) // used to determine if should rebroadcast
+
+	// FindTransaction looks for the provided TransactionIdentifier in processed
+	// blocks and returns the depth since the most recent sighting.
+	FindTransaction(context.Context, *types.TransactionIdentifier) (*types.BlockIdentifier, int64, error) // used to confirm
+
+	// BroadcastTransaction broadcasts a transaction to a Rosetta implementation
+	// and returns the *types.TransactionIdentifier returned by the implementation.
 	BroadcastTransaction(context.Context, string) (*types.TransactionIdentifier, error) // handle initial broadcast + confirm matches provided + rebroadcast if stale
 }
 
+// BroadcastStorageHandler is invoked when a transaction is confirmed on-chain
+// or when a transaction is considered stale.
 type BroadcastStorageHandler interface {
-	TransactionConfirmed(context.Context, *types.Transaction, []*types.Operation) error // can use locked account again + confirm matches intent + update logger
-	TransactionRebroadcast(context.Context, *types.TransactionIdentifier) error         // log in counter
+	// TransactionConfirmed is called when a transaction is observed on-chain for the
+	// last time at a block height < current block height - confirmationDepth.
+	TransactionConfirmed(context.Context, *types.BlockIdentifier, *types.Transaction, []*types.Operation) error // can use locked account again + confirm matches intent + update logger
+
+	// TransactionStale is called when a transaction has not yet been
+	// seen on-chain and is considered stale. This occurs when
+	// current block height - last broadcast > staleDepth.
+	TransactionStale(context.Context, *types.TransactionIdentifier) error // log in counter (rebroadcast should occur here)
 }
 
-type Broadcast struct {
+// broadcast is persisted to the db to track transaction broadcast.
+type broadcast struct {
 	Identifier    *types.TransactionIdentifier `json:"identifier"`
 	Intent        []*types.Operation           `json:"intent"`
+	Payload       string                       `json:"payload"`
 	LastBroadcast *types.BlockIdentifier       `json:"broadcast_at"`
 }
 
@@ -64,6 +84,8 @@ func NewBroadcastStorage(
 	}
 }
 
+// Initialize adds a BroadcastStorageHelper and BroadcastStorageHandler to BroadcastStorage.
+// This must be called prior to syncing!
 func (b *BroadcastStorage) Initialize(helper BroadcastStorageHelper, handler BroadcastStorageHandler) {
 	b.helper = helper
 	b.handler = handler
@@ -89,9 +111,20 @@ func (b *BroadcastStorage) RemovingBlock(
 	return nil, nil
 }
 
-func (b *BroadcastStorage) Broadcast(ctx context.Context, transactionIdentifier *types.TransactionIdentifier) error {
+// Broadcast is called when a caller wants a transaction to be broadcast and tracked.
+// The caller SHOULD NOT broadcast the transaction before calling this function.
+func (b *BroadcastStorage) Broadcast(
+	ctx context.Context,
+	intent []*types.Operation,
+	transactionIdentifier *types.TransactionIdentifier,
+	payload string,
+) error {
 	return errors.New("not implemented")
 }
+
+// LockedAddresses returns all addresses currently broadcasting a transaction.
+// The caller SHOULD NOT broadcast a transaction from an account if it is
+// considered locked!
 func (b *BroadcastStorage) LockedAddresses() error {
 	return errors.New("not implemented")
 }
