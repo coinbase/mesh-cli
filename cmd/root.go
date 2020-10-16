36e46996fd8a794b7f134cf555887cc33d69d1d5
Patrick O'Grady <prohb125@gmail.com>
2020-10-16 09:32:33 -0700

    Update version

--- cmd/root.go
+++ cmd/root.go
@@ -247,6 +247,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print rosetta-cli version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.5.13")
+		fmt.Println("v0.5.14")
 	},
 }
