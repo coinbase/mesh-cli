428f4af6362daa1eb2389939629054a76c6e8103
Madhur Shrimal <madhur.shrimal@gmail.com>
2021-09-07 19:54:09 -0700

    fix xgo compilation with new library

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -16,12 +16,21 @@
 
 VERSION=$1;
 
-xgo -go go-1.15.5 --targets=darwin/*,windows/*,linux/* -out "bin/rosetta-cli-${VERSION}" .;
+go get github.com/crazy-max/xgo
+
+MAC_TARGETS="darwin/amd64,darwin/arm64"
+LINUX_TARGETS="linux/amd64,linux/arm64,linux/mips64,linux/mips64le,linux/ppc64le,linux/s390x"
+WINDOWS_TARGET="windows/amd64"
+TARGETS="${MAC_TARGETS},${LINUX_TARGETS},${WINDOWS_TARGET}"
+
+xgo -go 1.16.3 --targets=${TARGETS} -out "bin/rosetta-cli-${VERSION}" .;
 
 # Rename some files
-mv "bin/rosetta-cli-${VERSION}-darwin-10.6-amd64" "bin/rosetta-cli-${VERSION}-darwin-amd64" 
+mv "bin/rosetta-cli-${VERSION}-darwin-10.12-amd64" "bin/rosetta-cli-${VERSION}-darwin-amd64"
 mv "bin/rosetta-cli-${VERSION}-windows-4.0-amd64.exe" "bin/rosetta-cli-${VERSION}-windows-amd64"
 
 # Tar all files
 cd bin || exit;
 for i in *; do tar -czf "$i.tar.gz" "$i" && rm "$i"; done
+
+go mod tidy
