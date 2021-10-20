d9178fdbee541c402b4c38c101fc7bd0eb64de5d
Madhur Shrimal <madhur.shrimal@gmail.com>
2021-10-20 14:11:03 -0700

    pprof

--- main.go
+++ main.go
@@ -15,17 +15,48 @@
 package main
 
 import (
+	"flag"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/coinbase/rosetta-cli/cmd"
 
 	"github.com/fatih/color"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			color.Red("could not create CPU profile:", err.Error())
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			color.Red("could not start CPU profile", err.Error())
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	err := cmd.Execute()
 	if err != nil {
 		color.Red("Command Failed: %s", err.Error())
 		os.Exit(1)
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			color.Red("could not create memory profile:", err.Error())
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			color.Red("could not write memory profile", err.Error())
+		}
 	}
 }
