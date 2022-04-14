163058c76aae2b908004224acc43080bc7f8461e
raghavapamula <raj.pamula@coinbase.com>
2022-04-14 13:40:14 -0400

    checkpoint

--- cmd/check_perf.go
+++ cmd/check_perf.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	checkPerfCmd = &cobra.Command{
-		Use:   "check:benchmark",
+		Use:   "check:perf",
 		Short: "Benchmark performance of time-critical methods of rosetta-cli and rosetta-sdk",
 		Long: `This command can be used to benchmark the performance of time critical methods for rosetta-cli and rosetta-sdk.
 This is useful for optimizing the performance of commands like check:data and check:construction, which can take a long time to complete.
@@ -20,7 +20,7 @@ The performance improvement can be objectively compared.`,
 
 func runCheckPerfCmd(_ *cobra.Command, _ []string) error {
 	ctx, cancel := context.WithCancel(Context)
-	timeTaken := t.Bmark_Sync(ctx, cancel, Config, 30000)
+	timeTaken := t.Bmark_Sync(ctx, cancel, Config, 3000)
 	fmt.Printf("Total Time Taken for Sync Operations: %s", timeTaken)
 	return nil
 }
