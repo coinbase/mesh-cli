beead6c2e29ac2879bac74818b275131ae39ad2a
raghavapamula <raj.pamula@coinbase.com>
2022-04-15 12:36:06 -0400

    Added improved error handling for check:data

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,7 +5,6 @@ import "errors"
 // Data Errors
 
 var (
-	// Data check errors
 	ErrCheckStorageTipFailed = errors.New("unable to check storage tip")
 	ErrDataCheckHalt         = errors.New("data check halted")
 	ErrInitDataTester        = errors.New("unexpected error occurred while trying to initialize data tester")
