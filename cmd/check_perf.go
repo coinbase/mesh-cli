fc69230d51fb22a49d172600faf59a8cd99430f3
raghavapamula <raj.pamula@coinbase.com>
2022-04-20 16:16:34 -0400

    Updated benchmarking code

--- cmd/check_perf.go
+++ cmd/check_perf.go
@@ -10,17 +10,16 @@ import (
 var (
 	checkPerfCmd = &cobra.Command{
 		Use:   "check:perf",
-		Short: "Benchmark performance of time-critical methods of rosetta-cli and rosetta-sdk",
-		Long: `This command can be used to benchmark the performance of time critical methods for rosetta-cli and rosetta-sdk.
-This is useful for optimizing the performance of commands like check:data and check:construction, which can take a long time to complete.
-The performance improvement can be objectively compared.`,
+		Short: "Benchmark performance of time-critical endpoints of Asset Issuer's Rosetta Implementation",
+		Long: `This command can be used to benchmark the performance of time critical methods for a rosetta server.
+This is useful for ensuring that there are no performance degradations in the rosetta-server.`,
 		RunE: runCheckPerfCmd,
 	}
 )
 
 func runCheckPerfCmd(_ *cobra.Command, _ []string) error {
 	ctx, cancel := context.WithCancel(Context)
-	
+
 	timeTaken := t.Bmark_Block(ctx, cancel, Config, 3000)
 	fmt.Printf("Total Time Taken for /block endpoint: %s", timeTaken)
 
