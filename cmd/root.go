8d2cd993136a0f795aeea70de62c015e109259c7
shiatcb <93357037+shiatcb@users.noreply.github.com>
2022-05-02 13:45:51 -0400

    ROSE-251: rosetta-cli version should return latest release (#298)

--- cmd/root.go
+++ cmd/root.go
@@ -295,6 +295,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print rosetta-cli version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.7.3")
+		fmt.Println("v0.7.6")
 	},
 }
